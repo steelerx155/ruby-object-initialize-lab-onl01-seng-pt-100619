5e7d492d5160985053cf3f877e52cb33b5c1becf
Randy Steele <steelerx155@gmail.com>
2019-10-23 17:25:57 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,20 +4,3 @@ class Person
  end
 end 
 
-# class Person
-#   def initialize(name)
-#     @name=(name)
-#   end
-# end
-
-
- 
-# def name=(name)
-#   @name = name 
-# end
- 
-# def
-# name
-# end
-
-# end 
